e0f98f39e449d00d9b2e53a99afd96016b6fa500
VKU-Esport <88511111+VKU-Esport@users.noreply.github.com>
2021-08-06 02:48:00 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>Necromancers - eSports Team &amp; Gaming HTML Template</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="description" content="eSports Team &amp; Gaming HTML Template">
+      <meta name="author" content="Dan Fisher">
+      <meta name="keywords" content="esports team news HTML template">
+      <link rel="shortcut icon" href="assets/img/favicons/favicon.ico">
+      <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/favicon-120.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/favicon-152.png">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <link href="https://fonts.googleapis.com/css?family=Aria:300,400,500,600,700" rel="stylesheet">
+      <link href="assets/vendor/magnific-popup/css/magnific-popup.css" rel="stylesheet">
+      <link href="assets/vendor/slick/css/slick.css" rel="stylesheet">
+      <link href="assets/vendor/nanoscroller/css/nanoscroller.css" rel="stylesheet">
+      <link href="assets/vendor/fontawesome/css/brands.css" rel="stylesheet">
+      <link href="assets/css/style.css" rel="stylesheet">
+      <link href="assets/css/custom.css" rel="stylesheet">
+   </head>
+   <body class="site-layout--horizontal preloader-is--active">
+      <div class="site-wrapper">
+		<?php include "header.html" ?>
+         <main class="site-content" id="wrapper">
+            <div class="widget-area widger-area--before-loop">
+               <div class="widget widget-carousel slick-slider">
+                  <article class="widget-carousel__item player bg-image bg--ph-03">
+                     <div class="player__subtitle h6">Chào mừng</div>
+                     <h1 class="player__title h3 color-primary">Đấng tối cao Yasou</h1>
+                     <div class="player__body">
+                        <div class="h5">đến với <span class="color-primary">VKU-Esport</span> team!</div>
+                     </div>
+                     <div class="player__thumbnail"><img src="assets/img/samples/yasou-removebg-preview.png" alt=""></div>
+                  </article>
+                  <article class="widget-carousel__item player bg-image bg--ph-06">
+                     <div class="player__subtitle h6">Kẻ có KDA cao nhất</div>
+                     <h1 class="player__title h3 color-warning">Darkin QQQtrox :V</h1>
+                     <div class="player__body">
+                        <ul class="player__stats">
+                           <li class="counter">
+                              <div class="counter__number">28</div>
+                              <div class="counter__label">Games</div>
+                           </li>
+                           <li class="counter">
+                              <div class="counter__number">73</div>
+                              <div class="counter__label">Kills</div>
+                           </li>
+                           <li class="counter">
+                              <div class="counter__number">96</div>
+                              <div class="counter__label">Win %</div>
+                           </li>
+                        </ul>
+                     </div>
+                     <div class="player__thumbnail"><img src="assets/img/samples/Aatrox_Winged_Render.png" alt=""></div>
+                  </article>
+               </div>
+            </div>
+            <div class="content blog-layout--style-1">
+               <article class="post has-post-thumbnail">
+                  <div class="post__thumbnail"><a href="blog-post.html"><img src="assets/img/samples/vex.jpg" alt=""></a></div>
+                  <div class="post__body">
+                     <div class="post__header">
+                        <ul class="post__cats list-unstyled">
+                           <li class="post__cats-item color-warning"><a href="#">Gaming</a></li>
+                        </ul>
+                        <h2 class="post__title h4"><a href="blog-post.html">Liên Minh Huyền Thoại ra tướng mới - Vex</a></h2>
+                        <ul class="post__meta list-unstyled">
+                           <li class="post__meta-item post__meta-item--date"><a href="#">31/8/2021</a></li>
+                        </ul>
+                     </div>
+                     <div class="post__excerpt">Cô nàng này sẽ sử dụng bóng tối của mình để thống trị và hủy diệt đối thủ. Riêng về cốt truyện của Vex, cô được biết đến là người chỉ muốn ở một mình từ bé (đặc tính của Yordle luôn cần ở cùng nhau và ở một mình như là bị tra tấn vậy). Trong cốt truyện Suy Vong, Vex sẽ đóng vai trò rất lớn ở phe ác cùng Viego với sức mạnh không ai biết tới.</div>
+                  </div>
+               </article>
+               <article class="post has-post-thumbnail post--video">
+                  <div class="post__thumbnail">
+                     <a href="https://www.youtube.com/watch?v=ySudxy00Qc8" class="mp_iframe">
+                        <img src="assets/img/samples/TahmKench_3.jpg" alt="">
+                        <div class="post__thumbnail-icon">
+                           <span class="lightbox__icon">
+                              <!---->
+                           </span>
+                        </div>
+                     </a>
+                  </div>
+                  <div class="post__body">
+                     <div class="post__header">
+                        <ul class="post__cats list-unstyled">
+                           <li class="post__cats-item color-danger"><a href="#">Videos</a></li>
+                        </ul>
+                        <h2 class="post__title h4"><a href="blog-post.html">"Khẩu vị độc đáo"</a></h2>
+                        <ul class="post__meta list-unstyled">
+                           <li class="post__meta-item post__meta-item--date"><a href="#">2/9/2021</a></li>
+                        </ul>
+                     </div>
+                     <div class="post__excerpt">Câu chuyện kể về một con cá trê tội nghiệp bị đánh hội đồng</div>
+                  </div>
+               </article>
+               <article class="post has-post-thumbnail">
+                  <div class="post__thumbnail"><a href="blog-post.html"><img src="assets/img/samples/vex.jpg" alt=""></a></div>
+                  <div class="post__body">
+                     <div class="post__header">
+                        <ul class="post__cats list-unstyled">
+                           <li class="post__cats-item color-warning"><a href="#">Gaming</a></li>
+                        </ul>
+                        <h2 class="post__title h4"><a href="blog-post.html">Liên Minh Huyền Thoại ra tướng mới - Vex</a></h2>
+                        <ul class="post__meta list-unstyled">
+                           <li class="post__meta-item post__meta-item--date"><a href="#">31/8/2021</a></li>
+                        </ul>
+                     </div>
+                     <div class="post__excerpt">Cô nàng này sẽ sử dụng bóng tối của mình để thống trị và hủy diệt đối thủ. Riêng về cốt truyện của Vex, cô được biết đến là người chỉ muốn ở một mình từ bé (đặc tính của Yordle luôn cần ở cùng nhau và ở một mình như là bị tra tấn vậy). Trong cốt truyện Suy Vong, Vex sẽ đóng vai trò rất lớn ở phe ác cùng Viego với sức mạnh không ai biết tới.</div>
+                  </div>
+               </article>
+               <article class="post has-post-thumbnail">
+                  <div class="post__thumbnail"><a href="blog-post.html"><img src="assets/img/samples/vex.jpg" alt=""></a></div>
+                  <div class="post__body">
+                     <div class="post__header">
+                        <ul class="post__cats list-unstyled">
+                           <li class="post__cats-item color-warning"><a href="#">Gaming</a></li>
+                        </ul>
+                        <h2 class="post__title h4"><a href="blog-post.html">Liên Minh Huyền Thoại ra tướng mới - Vex</a></h2>
+                        <ul class="post__meta list-unstyled">
+                           <li class="post__meta-item post__meta-item--date"><a href="#">31/8/2021</a></li>
+                        </ul>
+                     </div>
+                     <div class="post__excerpt">Cô nàng này sẽ sử dụng bóng tối của mình để thống trị và hủy diệt đối thủ. Riêng về cốt truyện của Vex, cô được biết đến là người chỉ muốn ở một mình từ bé (đặc tính của Yordle luôn cần ở cùng nhau và ở một mình như là bị tra tấn vậy). Trong cốt truyện Suy Vong, Vex sẽ đóng vai trò rất lớn ở phe ác cùng Viego với sức mạnh không ai biết tới.</div>
+                  </div>
+               </article>
+               <article class="post has-post-thumbnail">
+                  <div class="post__thumbnail"><a href="blog-post.html"><img src="assets/img/samples/vex.jpg" alt=""></a></div>
+                  <div class="post__body">
+                     <div class="post__header">
+                        <ul class="post__cats list-unstyled">
+                           <li class="post__cats-item color-warning"><a href="#">Gaming</a></li>
+                        </ul>
+                        <h2 class="post__title h4"><a href="blog-post.html">Liên Minh Huyền Thoại ra tướng mới - Vex</a></h2>
+                        <ul class="post__meta list-unstyled">
+                           <li class="post__meta-item post__meta-item--date"><a href="#">31/8/2021</a></li>
+                        </ul>
+                     </div>
+                     <div class="post__excerpt">Cô nàng này sẽ sử dụng bóng tối của mình để thống trị và hủy diệt đối thủ. Riêng về cốt truyện của Vex, cô được biết đến là người chỉ muốn ở một mình từ bé (đặc tính của Yordle luôn cần ở cùng nhau và ở một mình như là bị tra tấn vậy). Trong cốt truyện Suy Vong, Vex sẽ đóng vai trò rất lớn ở phe ác cùng Viego với sức mạnh không ai biết tới.</div>
+                  </div>
+               </article>
+               
+               <article class="post has-post-thumbnail">
+                  <div class="post__thumbnail"><a href="blog-post.html"><img src="assets/img/samples/vex.jpg" alt=""></a></div>
+                  <div class="post__body">
+                     <div class="post__header">
+                        <ul class="post__cats list-unstyled">
+                           <li class="post__cats-item color-warning"><a href="#">Gaming</a></li>
+                        </ul>
+                        <h2 class="post__title h4"><a href="blog-post.html">Liên Minh Huyền Thoại ra tướng mới - Vex</a></h2>
+                        <ul class="post__meta list-unstyled">
+                           <li class="post__meta-item post__meta-item--date"><a href="#">31/8/2021</a></li>
+                        </ul>
+                     </div>
+                     <div class="post__excerpt">Cô nàng này sẽ sử dụng bóng tối của mình để thống trị và hủy diệt đối thủ. Riêng về cốt truyện của Vex, cô được biết đến là người chỉ muốn ở một mình từ bé (đặc tính của Yordle luôn cần ở cùng nhau và ở một mình như là bị tra tấn vậy). Trong cốt truyện Suy Vong, Vex sẽ đóng vai trò rất lớn ở phe ác cùng Viego với sức mạnh không ai biết tới.</div>
+                  </div>
+               </article>
+            </div>
+         </main>
+         <div class="site-overlay"></div>
+      </div>
+      <div class="preloader-overlay">
+         <div id="js-preloader" class="preloader">
+            <div class="preloader-inner fadeInUp">
+               <div class="pong-loader"></div>
+               <svg role="img" class="df-icon df-icon--preloader-arcade">
+                  <use xlink:href="assets/img/necromancers.svg#preloader-arcade"/>
+               </svg>
+            </div>
+         </div>
+      </div>
+      <!-- The cursor elements -->
+      <div class="df-custom-cursor-wrap">
+         <div id="df-custom-cursor"></div>
+      </div>
+      <!-- Javascript Files
+         ================================================== --><!-- Core JS --><script src="assets/vendor/jquery/jquery.min.js"></script><script src="assets/vendor/jquery/jquery-migrate.min.js"></script><script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script><script src="assets/js/core.js"></script><!-- Vendor JS --><!-- Template JS --><script src="assets/js/init.js"></script><script src="assets/js/custom.js"></script><!-- Duotone SVG color filter --> 
+      <svg xmlns="http://www.w3.org/2000/svg" class="svg-filters">
+         <filter id="duotone_base">
+            <feColorMatrix type="matrix" result="grayscale" values="1 0 0 0 0
+               1 0 0 0 0
+               1 0 0 0 0
+               0 0 0 1 0"/>
+            <feComponentTransfer color-interpolation-filters="sRGB" result="duotone_base_filter">
+               <feFuncR type="table" tableValues="0.082352941176471 0.419607843137255"></feFuncR>
+               <feFuncG type="table" tableValues="0.090196078431373 0.443137254901961"></feFuncG>
+               <feFuncB type="table" tableValues="0.125490196078431 0.6"></feFuncB>
+               <feFuncA type="table" tableValues="0 1"></feFuncA>
+            </feComponentTransfer>
+         </filter>
+      </svg>
+   </body>
+   <!-- Mirrored from necromancers.dan-fisher.dev/home.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 05 Aug 2021 16:49:32 GMT -->
+</html>
---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>Necromancers - eSports Team &amp; Gaming HTML Template</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="description" content="eSports Team &amp; Gaming HTML Template">
+      <meta name="author" content="Dan Fisher">
+      <meta name="keywords" content="esports team news HTML template">
+      <link rel="shortcut icon" href="assets/img/favicons/favicon.ico">
+      <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/favicon-120.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/favicon-152.png">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <link href="https://fonts.googleapis.com/css?family=Helvetica:300,400,500,600,700" rel="stylesheet">
+      <link href="assets/vendor/magnific-popup/css/magnific-popup.css" rel="stylesheet">
+      <link href="assets/vendor/slick/css/slick.css" rel="stylesheet">
+      <link href="assets/vendor/nanoscroller/css/nanoscroller.css" rel="stylesheet">
+      <link href="assets/vendor/fontawesome/css/brands.css" rel="stylesheet">
+      <link href="assets/css/style.css" rel="stylesheet">
+      <link href="assets/css/custom.css" rel="stylesheet">
+   </head>
+   <body class="site-layout--landing preloader-is--active bg-image bg-fixed bg--texture-05">
+      <div class="site-wrapper">
+         <header id="header" class="site-header site-header--landing">
+            <nav class="main-nav">
+               <ul class="main-nav__list">
+                  <li class=""><a href="home.php">Trang chủ</a></li>
+                  <li class="">
+                     <a href="#">Chơi game Offline</a>
+                     <ul class="main-nav__sub">
+                        <li class="">
+							<a href="https://quocvinhvku.herokuapp.com/">Mê Cung RPG</a>
+                        </li>
+                        <li class="">
+							<a href="https://quocvinhvku.github.io/">Game Nhái Mario</a>
+                        </li>
+                     </ul>
+                  </li>
+               </ul>
+            </nav>
+            <div class="header-logo header-logo--img"><a href="index.php"><img src="assets/img/logo.png" srcset="assets/img/logo@2x.png 2x" alt="Necromancers"></a></div>
+            <nav class="main-nav">
+               <ul class="main-nav__list">
+                  <li class="">
+                     <a href="blog.php">Mới nhất</a>
+                    
+                  </li>
+                  <li class="">
+                     <a href="#">Donate</a>
+                     <ul class="main-nav__sub">
+                        <li class=""><a href="https://www.facebook.com/hoang.kyns">Ngô Quý Hiếu</a></li>
+                     </ul>
+                  </li>
+               </ul>
+            </nav>
+         </header>
+         <main class="site-content text-center" id="wrapper">
+            <div class="site-content__center">
+               <h1 class="text-white landing-title"><span class="subtitle landing-subtitle">VKU Esport - Nơi hội tụ đam mê thể thao điện tử </span>VKU ESPORT</h1>
+               <a href="home.php" class="btn btn-primary btn-lg btn--landing"><span>Start</span></a>
+            </div>
+         </main>
+         <footer id="footer" class="footer text-center">
+            <ul class="social-menu social-menu--landing social-menu--landing-glitch">
+               <li><a href="https://www.facebook.com/clbesportvku/" target="_blank"><i class="fab fa-facebook-square"></i> <span class="link-subtitle">Facebook</span>VKU-Esport</a></li>
+               <li><a href="#" target="_blank"><i class="fab fa-twitter"></i> <span class="link-subtitle">Twitter</span>VKU-Esport</a></li>
+               <li><a href="#" target="_blank"><i class="fab fa-twitch"></i> <span class="link-subtitle">Twitch</span>VKU-Esport</a></li>
+               <li><a href="#" target="_blank"><i class="fab fa-discord"></i> <span class="link-subtitle">Discord</span>VKU-Esport</a></li>
+            </ul>
+         </footer>
+         <!-- Footer / End -->
+         <div class="landing-detail landing-detail--left"><span>&nbsp;</span> <span>&nbsp;</span> <span>&nbsp;</span></div>
+         <div class="landing-detail-cover landing-detail-cover--left"><span>&nbsp;</span> <span>&nbsp;</span> <span>&nbsp;</span></div>
+         <div class="landing-detail landing-detail--right"><span>&nbsp;</span> <span>&nbsp;</span> <span>&nbsp;</span></div>
+         <div class="landing-detail-cover landing-detail-cover--right"><span>&nbsp;</span> <span>&nbsp;</span> <span>&nbsp;</span></div>
+         <!-- Overlay -->
+         <div class="site-overlay"></div>
+         <div class="search-panel">
+            <div class="search-panel__content">
+               <form action="#" id="header-search-form" class="search-form search-form--header"><input type="text" class="form-control" value="" placeholder="What are you looking for...?"></form>
+               <span>Write what you are looking for, choose the filters and press enter to begin your search!</span>
+            </div>
+         </div>
+         
+      <div class="preloader-overlay">
+         <div id="js-preloader" class="preloader">
+            <div class="preloader-inner fadeInUp">
+               <div class="pong-loader"></div>
+               <svg role="img" class="df-icon df-icon--preloader-arcade">
+                  <use xlink:href="assets/img/necromancers.svg#preloader-arcade"/>
+               </svg>
+            </div>
+         </div>
+      </div>
+      <!-- The cursor elements -->
+      <div class="df-custom-cursor-wrap">
+         <div id="df-custom-cursor"></div>
+      </div>
+      <script src="assets/vendor/jquery/jquery.min.js"></script><script src="assets/vendor/jquery/jquery-migrate.min.js"></script><script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script><script src="assets/js/core.js"></script><!-- Vendor JS --><!-- Template JS --><script src="assets/js/init.js"></script><script src="assets/js/custom.js"></script><!-- Duotone SVG color filter --> 
+      <svg xmlns="http://www.w3.org/2000/svg" class="svg-filters">
+         <filter id="duotone_base">
+            <feColorMatrix type="matrix" result="grayscale" values="1 0 0 0 0
+               1 0 0 0 0
+               1 0 0 0 0
+               0 0 0 1 0"/>
+            <feComponentTransfer color-interpolation-filters="sRGB" result="duotone_base_filter">
+               <feFuncR type="table" tableValues="0.082352941176471 0.419607843137255"></feFuncR>
+               <feFuncG type="table" tableValues="0.090196078431373 0.443137254901961"></feFuncG>
+               <feFuncB type="table" tableValues="0.125490196078431 0.6"></feFuncB>
+               <feFuncA type="table" tableValues="0 1"></feFuncA>
+            </feComponentTransfer>
+         </filter>
+      </svg>
+   </body>
+</html>
