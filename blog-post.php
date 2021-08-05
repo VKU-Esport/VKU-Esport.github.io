fd3158b896eee5e340d1291a4e8b3d0d384aabc8
quocvinhvku <dqvinh.19it2@vku.udn.vn>
2021-08-06 02:49:13 +0700

    asset

---
+++ blog-post.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   
+   <head>
+      <title>Necromancers - eSports Team &amp; Gaming HTML Template</title>
+      <meta charset="utf-8">
+      <link rel="shortcut icon" href="assets/img/favicons/favicon.ico">
+      <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/favicon-120.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/favicon-152.png">
+     
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+
+      <link href="https://fonts.googleapis.com/css?family=Aria:300,400,500,600,700" rel="stylesheet">
+      <link href="assets/vendor/magnific-popup/css/magnific-popup.css" rel="stylesheet">
+      <link href="assets/vendor/slick/css/slick.css" rel="stylesheet">
+      <link href="assets/vendor/nanoscroller/css/nanoscroller.css" rel="stylesheet">
+      <link href="assets/vendor/fontawesome/css/brands.css" rel="stylesheet">
+      <!-- Template CSS-->
+      <link href="assets/css/style.css" rel="stylesheet">
+      <!-- Custom CSS-->
+      <link href="assets/css/custom.css" rel="stylesheet">
+   </head>
+   <body class="preloader-is--active">
+      <div class="site-wrapper site-layout--default">
+         <!-- Header
+            ================================================== -->
+         <?php include "header.html" ?>
+         <main class="site-content" id="wrapper">
+            <div class="site-content__inner">
+               <div class="site-content__holder">
+                  <!-- Post -->
+                  <article class="post post--single">
+                     <figure class="post__thumbnail"><img src="assets/img/samples/vex.jpg" alt=""></figure>
+                     <ul class="post__sharing">
+                        <li class="post__sharing-item post__sharing-item--menu"><a href="home.php"><i>&nbsp;</i></a></li>
+                        <li class="post__sharing-item"><a href="https://www.facebook.com/"></a></li>
+                        <li class="post__sharing-item"><a href="https://twitter.com/"></a></li>
+                        <li class="post__sharing-item post__sharing-item--comments">
+                           <a href="#comments">
+                              <span>4</span> 
+                              <svg role="img" class="df-icon df-icon--comments-small">
+                                 <use xlink:href="assets/img/necromancers.svg#comments-small"/>
+                              </svg>
+                           </a>
+                        </li>
+                     </ul>
+                     <div class="post__header">
+                        <div class="post__cats h6"><span class="color-warning">Gaming</span></div>
+                        <h2 class="post__title h3">New League of Legends champion Vex</h2>
+                        <div class="post__meta"><span class="meta-item meta-item--date">31/8/2021</span></div>
+                     </div>
+                     <div class="post__body">
+                        <p>Nhà vô địch Liên Minh Huyền Thoại tiếp theo, pháp sư u ám, đã được công bố sớm, với thông tin rò rỉ về Sentinels of Light mới tiết lộ Vex ⁠— trước đây được cho là "Norra" ⁠— sẽ trở thành nhân vật LoL mới nhất ra mắt trên Summoner's Rift trong Phần 11 .</p>
+                        <p>Cuối năm ngoái, Riot Games đã hứa mọi thứ liên quan đến Liên Minh Huyền Thoại ⁠— các sự kiện, bản phát hành tướng và cập nhật ⁠— sẽ liên quan đến một sự kiện trên toàn trò chơi, "Sentinels of Light" . Cho đến nay họ vẫn giữ lời hứa với Viego, Gwen và Akshan là những nhân vật chính trong cốt truyện Ruination.</p>
+                        <figure>
+                           <img src="assets/img/samples/vex2.png" alt="">
+                           <figcaption>Đây là tất cả những gì chúng ta biết về nhân vật thứ 157 của LoL.</figcaption>
+                        </figure>
+                        <p>Vex, Gloomy Mage là ai?
+Hiện tại rất ít thông tin cụ thể về “Vex”.
+
+Tuy nhiên, có một giả thuyết phổ biến về Liên minh huyền thoại rằng Gloomy Mage là chủ nhân mất tích của Yuumi. Yordle biến mất bằng cách sử dụng Ngưỡng Sách (cuốn Yuumi bay lên) để "dịch chuyển đến một nơi nguy hiểm."
+
+Theo những mẩu tin truyền thuyết ban đầu và những rò rỉ của Riot , nhân vật này thích đi khám phá “trên khắp các lĩnh vực vật chất và tinh thần”.
+
+Riot ban đầu đã trêu chọc rằng pháp sư u ám sẽ "thà ở một mình". Các hình ảnh đi kèm mô tả một cánh cổng bên ngoài Thành phố Bandle đến Quần đảo Bóng tối, giống như một trong những Ngưỡng Sách có thể mở ra. Kể từ đó, thiết kế của nhà vô địch (và có thể là truyền thuyết) đã được thay đổi một chút.
+
+Tuy nhiên, Vex vẫn sẽ là một Yordle với “một phần quan trọng trong câu chuyện của Viego.”</p>
+                     </div>
+                  </article>
+                  <!-- Post / End --><!-- Post Comments -->
+                  <div class="post-comments" id="comments">
+                     <h4 class="post-comments__title">Comments (1)</h4>
+                     <ol class="comments">
+                        <li class="comment">
+                           <div class="comment__avatar"><img src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.6435-9/229983087_384875213231605_7154542279471304583_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=hReF40pEP5wAX_qrYFl&_nc_ht=scontent.fdad2-1.fna&oh=192a40d996af1b8bc827ec758fcbf9a2&oe=61336BEC" alt=""></div>
+                           <div class="comment__body">
+                              <h6 class="comment__author">QuocVinhVKU</h6>
+                              <p>Best Vex Ngũ Hành Sơn nhận mọi kèo ey</p>
+                              <div class="comment__meta">
+                                 <time class="comment__date">5 minutes ago</time>
+                                 <div class="comment__reply"><a href="#">Reply</a></div>
+                              </div>
+                           </div>
+                        </li>
+                        
+                     </ol>
+                  </div>
+                  <!-- Post Comments / End --><!-- Comment Form -->
+                  <div class="post-comments-form">
+                     <h4 class="post-comments-form__title">Leave a comment</h4>
+                     <form action="#" class="comment-form">
+                        <div class="row">
+                           <div class="col-md-6">
+                              <div class="form-group"><input type="text" class="form-control" id="input-name" placeholder="Your Name"></div>
+                           </div>
+                           <div class="col-md-6">
+                              <div class="form-group"><input type="text" class="form-control" id="input-email" placeholder="Your Email"></div>
+                           </div>
+                        </div>
+                        <div class="row">
+                           <div class="col-md-12">
+                              <div class="form-group"><textarea name="input-comment" cols="30" rows="5" class="form-control" id="input-comment" placeholder="Your Comment"></textarea></div>
+                           </div>
+                        </div>
+                        <button class="btn btn-secondary">Post Comment</button>
+                     </form>
+                  </div>
+                  <!-- Comment Form / End -->
+               </div>
+            </div>
+         </main>
+         <!-- Overlay -->
+         <div class="site-overlay"></div>
+         <!-- Overlay / End --><!-- Cart Panel -->
+         
+      </div>
+      <div class="preloader-overlay">
+         <div id="js-preloader" class="preloader">
+            <div class="preloader-inner fadeInUp">
+               <div class="pong-loader"></div>
+               <svg role="img" class="df-icon df-icon--preloader-arcade">
+                  <use xlink:href="assets/img/necromancers.svg#preloader-arcade"/>
+               </svg>
+            </div>
+         </div>
+      </div>
+      <!-- The cursor elements -->
+      <div class="df-custom-cursor-wrap">
+         <div id="df-custom-cursor"></div>
+      </div>
+      <script src="assets/vendor/jquery/jquery.min.js"></script><script src="assets/vendor/jquery/jquery-migrate.min.js"></script><script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script><script src="assets/js/core.js"></script><!-- Vendor JS --><!-- Template JS --><script src="assets/js/init.js"></script><script src="assets/js/custom.js"></script><!-- Duotone SVG color filter --> 
+      <svg xmlns="http://www.w3.org/2000/svg" class="svg-filters">
+         <filter id="duotone_base">
+            <feColorMatrix type="matrix" result="grayscale" values="1 0 0 0 0
+               1 0 0 0 0
+               1 0 0 0 0
+               0 0 0 1 0"/>
+            <feComponentTransfer color-interpolation-filters="sRGB" result="duotone_base_filter">
+               <feFuncR type="table" tableValues="0.082352941176471 0.419607843137255"></feFuncR>
+               <feFuncG type="table" tableValues="0.090196078431373 0.443137254901961"></feFuncG>
+               <feFuncB type="table" tableValues="0.125490196078431 0.6"></feFuncB>
+               <feFuncA type="table" tableValues="0 1"></feFuncA>
+            </feComponentTransfer>
+         </filter>
+      </svg>
+   </body>
+   </html>
